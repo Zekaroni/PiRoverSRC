dc5715ef0c1f169c4bd31921756df9e0fb8c39fc
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-04 00:09:31 -0400

    init for actual project

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from lib.controller import Controller
 from lib.piWrapper import Board
+from lib.piSerial import SerialOutput
 
+controller = Controller()
+piBoard = Board()
+arduinoInput = SerialOutput()
