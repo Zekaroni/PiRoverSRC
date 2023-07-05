1cf1c9d787c12d7f1c618c74ed4bec7a3db38801
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-05 01:54:27 -0400

    debug for controller test

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from lib.controller import Controller
-from lib.piWrapper import Board
 from lib.piSerial import SerialOutput
+# from lib.piWrapper import Board # Doesn't work on Windows, commented out for testing
+
+from time import sleep
 
 controller = Controller()
-piBoard = Board()
 arduinoInput = SerialOutput()
+# piBoard = Board()
+
+while 1:
+    print(controller.getValues())
+    sleep(0.1)
