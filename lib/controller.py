ff6e3c6f8d3ff2a031c5e2cb2daaa573b8773bba
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-05 18:12:26 -0400

    back to ds4drv

--- lib/controller.py
+++ lib/controller.py
@@ -1,56 +1,41 @@
-import evdev
+from ds4drv import DS4Dev
 import time
 import threading
 from queue import Queue
 
 RATE_LIMIT_PERIOD = 0.1  # Rate limit period in seconds
 
-def find_ds4_controller():
-    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-    for device in devices:
-        if "Wireless Controller" in device.name:
-            return device
-    return None
-
 def handle_event(event, result_queue):
-    if event.type == evdev.ecodes.EV_KEY:
-        key_event = evdev.categorize(event)
-        if key_event.keystate == 1:  # Key press event
-            result_queue.put("Button Pressed: " + str(key_event.keycode))
-        elif key_event.keystate == 0:  # Key release event
-            result_queue.put("Button Released: " + str(key_event.keycode))
-    # elif event.type == evdev.ecodes.EV_ABS:
-    #     if event.code == evdev.ecodes.ABS_X:
-    #         result_queue.put("Left Stick X-axis: " + str(event.value))
-    #     elif event.code == evdev.ecodes.ABS_Y:
-    #         result_queue.put("Left Stick Y-axis: " + str(event.value))
-    #     elif event.code == evdev.ecodes.ABS_RX:
-    #         result_queue.put("Right Stick X-axis: " + str(event.value))
-    #     elif event.code == evdev.ecodes.ABS_RY:
-    #         result_queue.put("Right Stick Y-axis: " + str(event.value))
-    #     elif event.code == evdev.ecodes.ABS_Z:  # Left trigger
-    #         result_queue.put("Left Trigger: " + str(event.value))
-    #     elif event.code == evdev.ecodes.ABS_RZ:  # Right trigger
-    #         result_queue.put("Right Trigger: " + str(event.value))
+    event_type = event['event_type']
+    if event_type == 'button':
+        button_name = event['button_name']
+        if event['value']:
+            result_queue.put("Button Pressed: " + button_name)
+        else:
+            result_queue.put("Button Released: " + button_name)
+    elif event_type == 'axis':
+        axis_name = event['axis_name']
+        axis_value = event['value']
+        result_queue.put(f"{axis_name}: {axis_value}")
 
-def event_handling_thread(device, result_queue):
-    for event in device.read_loop():
+def event_handling_thread(ds4dev, result_queue):
+    for event in ds4dev.events():
         handle_event(event, result_queue)
 
+# Create a DS4Dev object
+ds4dev = DS4Dev()
+
 # Find the DS4 controller
-ds4 = find_ds4_controller()
+ds4dev.find_ds4()
 
-if ds4 is not None:
-    print("DS4 controller found:", ds4.name)
-
-    # Create an event device for the DS4 controller
-    device = evdev.InputDevice(ds4.path)
+if ds4dev.connected:
+    print("DS4 controller found:", ds4dev.name)
 
     # Create a result queue for storing the event handling results
     result_queue = Queue()
 
     # Create and start the event handling thread
-    event_thread = threading.Thread(target=event_handling_thread, args=(device, result_queue))
+    event_thread = threading.Thread(target=event_handling_thread, args=(ds4dev, result_queue))
     event_thread.daemon = True  # Allow the program to exit even if this thread is running
     event_thread.start()
 
