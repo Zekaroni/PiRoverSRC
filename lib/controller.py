1a09d95580a1d1afbc5b93fd5c9bf132dc40d999
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-07-05 03:45:50 -0400

    swtiched to ds4drv instead of pygame due to error

--- lib/controller.py
+++ lib/controller.py
@@ -1,71 +1,25 @@
-import pygame
-from os import environ
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+from ds4drv import DS4drv
+import time
 
-class Controller:
-    def __init__(self):
-        pygame.init()
-        pygame.joystick.init()
-        self.joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-        for joystick in self.joysticks:
-            joystick.init()
+def handle_event(event):
+    if event.event_type == "button_press":
+        print("Button Pressed:", event.button)
+    elif event.event_type == "button_release":
+        print("Button Released:", event.button)
+    elif event.event_type == "axis_motion":
+        print("Axis Motion:", event.axis, event.value)
 
-        self._BUTTON_MAPPINGS = {
-            0: "X",
-            1: "CIRCLE",
-            2: "SQUARE",
-            3: "TRIANGLE",
-            4: "SHARE",
-            5: "PS",
-            6: "OPTIONS",
-            7: "L3",
-            8: "R3",
-            9: "L1",
-            10: "R1",
-            11: "UP",
-            12: "DOWN",
-            13: "LEFT",
-            14: "RIGHT",
-            15: "TOUCHPAD",
-        }
+def search_for_controller():
+    while True:
+        try:
+            ds4 = DS4drv(callback=handle_event)
+            ds4.start()
+            print("DS4 controller found. Listening for events...")
+            while True:
+                time.sleep(1)
+        except:
+            print("DS4 controller not found. Retrying in 5 seconds...")
+            time.sleep(5)
 
-        self._JOYSTICK_MAPPINGS ={
-            0: "LEFT_STICK_X",
-            1: "LEFT_STICK_Y",
-            2: "RIGHT_STICK_X",
-            3: "RIGHT_STICK_Y",
-            4: "L2",
-            5: "R2",
-        }
-
-        self._BUTTONS = {button_number: 0 for button_number in range(len(self._BUTTON_MAPPINGS))}
-        self._ANALOG_STICKS = {axis: (0.0 if axis < 4 else -1.0) for axis in range(len(self._JOYSTICK_MAPPINGS))}
-
-        self._DEADZONE = 0.2
-    
-    def updateState(self):
-        for event in pygame.event.get():
-            if event.type == pygame.JOYAXISMOTION:
-                axis = event.axis
-                value = event.value
-                if axis in range(4):
-                    if value < self._DEADZONE and value > -self._DEADZONE:
-                        value = 0.0
-                self._ANALOG_STICKS[axis] = round(value, 3)
-            elif event.type == pygame.JOYBUTTONDOWN:
-                button = event.button
-                self._BUTTONS[button] = 1
-            elif event.type == pygame.JOYBUTTONUP:
-                button = event.button
-                self._BUTTONS[button] = 0    
-
-    def getValues(self):
-        values = {}
-        for i in range(len(self._JOYSTICK_MAPPINGS)):
-            values[self._JOYSTICK_MAPPINGS[i]] = self._ANALOG_STICKS[i]
-        for i in range(len(self._BUTTON_MAPPINGS)):
-            values[self._BUTTON_MAPPINGS[i]] = self._BUTTONS[i]
-        return values
-    
-    def setDeadzone(self, deadzone: float):
-        self._DEADZONE = deadzone
+# Start the search loop
+search_for_controller()
